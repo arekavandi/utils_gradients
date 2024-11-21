172f74579244e03bb34a85a928485bacc06506c9
Aref Miri Rekavandi <101369948+arekavandi@users.noreply.github.com>
2024-11-21 10:37:36 +0000

    Update utils_gradients.py

--- functions/utils_gradients.py
+++ functions/utils_gradients.py
@@ -193,4 +193,154 @@ def compute_distance_matrix(data):
     
     return distance_matrix.flatten()
 
+def display_columns(matrix, Nv1, Nv2,type):
+    """
+    Function to visualize columns of a matrix in m separate plots in with shape of Nv1*Nv2 image.
+
+    Parameters:
+    matrix (numpy.ndarray): matrix of shape (Nv1*Nv2, m).
+    Nv1 (integer): Width of the plotted image.
+    Nv2 (integer): Height of the plotted image.
+
+    Returns:
+    Just the plot.
+    """
+    A, B = matrix.shape
+    reshaped_columns = [np.reshape(matrix[:, i], (Nv1, Nv2)) for i in range(B)]
+    
+    fig, axs = plt.subplots(1, B, figsize=(B * 3, 3))  # Adjust figsize as needed
+    for i, col in enumerate(reshaped_columns):
+        axs[i].imshow(col, cmap=type)  # Change the colormap if needed
+        axs[i].set_title(f'PC {i+1}')
+        axs[i].axis('off')
+    plt.show()
+
+def display_compare(temp,Dense):
+    """
+    Function to visualize matrix temp and its zoomed version in comparision with actual dense connectome.
+
+    Returns:
+    Just the plot.
+    """
+    zoom=3
+    fig, axs = plt.subplots(1, 2, figsize=(7, 3))
+    
+    im_1=axs[0].imshow(Dense, interpolation='nearest')
+    fig.colorbar(im_1, ax=axs[0], orientation='vertical')
+    axs[0].set_title('DC')
+    axins = zoomed_inset_axes(axs[0], zoom, loc=1) # zoom = 6
+    axins.imshow(Dense, interpolation="nearest",
+             origin="lower")
+
+    # sub region of the original image
+    x1, x2, y1, y2 = 300, 100, 100, 300
+    axins.set_xlim(x1, x2)
+    axins.set_ylim(y1, y2)
+
+    plt.xticks(visible=False)
+    plt.yticks(visible=False)
+
+    # draw a bbox of the region of the inset axes in the parent axes and
+    # connecting lines between the bbox and the inset axes area'''
+    mark_inset(axs[0], axins, loc1=2, loc2=4, fc="none", ec="0.5")
+
+    plt.draw()
+  
+
+    im_2=axs[1].imshow(temp, interpolation='nearest')
+    fig.colorbar(im_2, ax=axs[1], orientation='vertical')
+    axs[1].set_title('Approx. DC')
+
+    axins = zoomed_inset_axes(axs[1], zoom, loc=1) # zoom = 6
+    axins.imshow(temp, interpolation="nearest",
+             origin="lower")
+    
+    axins.set_xlim(x1, x2)
+    axins.set_ylim(y1, y2)
+
+    plt.xticks(visible=False)
+    plt.yticks(visible=False)
+
+    # draw a bbox of the region of the inset axes in the parent axes and
+    # connecting lines between the bbox and the inset axes area'''
+    mark_inset(axs[1], axins, loc1=2, loc2=4, fc="none", ec="0.5")
+
+    plt.draw()
+    
+def to_index(x, y, width):
+    """
+    to convert 2D indices to 1D index
+
+    Parameters:
+    x (integer): x coordinate of the point.
+    y (integer): y coordinate of the point.
+    width (integer): Height of the voxel slice.
+
+    Returns:
+    integer: The encoded index in 1D array.
+    """
+    return x * width + y
+
+def create_graph(matrix):
+    Nv1, Nv2 = matrix.shape
+    num_nodes = Nv1 * Nv2
+    indices = []
+    indptr = [0]
+    data = []
+
+    for i in range(Nv1):
+        for j in range(Nv2):
+            if matrix[i, j] == 1:  # Only consider spiral points
+                node_index = to_index(i, j, Nv2)
+                neighbors = []
+                if i > 0 and matrix[i-1, j] == 1:
+                    neighbors.append(to_index(i-1, j, Nv2))
+                if i < Nv1 - 1 and matrix[i+1, j] == 1:
+                    neighbors.append(to_index(i+1, j, Nv2))
+                if j > 0 and matrix[i, j-1] == 1:
+                    neighbors.append(to_index(i, j-1, Nv2))
+                if j < Nv2 - 1 and matrix[i, j+1] == 1:
+                    neighbors.append(to_index(i, j+1, Nv2))
+
+                indices.extend(neighbors)
+                data.extend([1] * len(neighbors))
+            indptr.append(len(indices))
+    
+    return csr_matrix((data, indices, indptr), shape=(num_nodes, num_nodes))
+    
+
+def find_spiral_points(matrix):
+    points = np.argwhere(matrix == 1)
+    return points
+
+def dist_vs_dist(mask,main_comp):
+    """
+    function to compare the distiance on a manifold (original space) with the distance in embedding space.
+
+    Parameters:
+    mask (numpy.ndarray): Manifold mask of shape (Nv1, Nv2)
+    main_comp (numpy.ndarray): embedding of the points with the shape (Nv1*Nv2,1).
+
+    Returns:
+    Manifold_distance vs embedding distance correlation scatter plot.
+    """    
+    graph = create_graph(mask)
+    Mss_points = find_spiral_points(mask)
+    distance_manifold=[]
+    distance_embedding=[]
+    for i in range(Mss_points.shape[0]):
+        point1 = Mss_points[i]
+        for j in range(Mss_points.shape[0]):
+            point2 = Mss_points[j]
+            index1 = to_index(point1[0], point1[1], Nv2)
+            index2 = to_index(point2[0], point2[1], Nv2)
+            dist_matrix, predecessors = dijkstra(csgraph=graph, directed=False, indices=index1, return_predecessors=True)
+            distance_manifold .append(dist_matrix[index2])
+            distance_embedding.append(np.abs(np.reshape(main_comp,(Nv1,Nv2))[Mss_points[i][0],Mss_points[i][1]]-np.reshape(main_comp,(Nv1,Nv2))[Mss_points[j][0],Mss_points[j][1]]))
+    plt.scatter(distance_manifold,distance_embedding, alpha=0.2)
+    plt.xlabel('Manifold_distance')
+    plt.ylabel('Embedding_distance')
+    print(f'Dist_to_Dist Correlation:{np.corrcoef(distance_manifold,distance_embedding)[0,1]}')
+
+
 
