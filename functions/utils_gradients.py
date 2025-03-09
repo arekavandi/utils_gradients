524abd6b052f38ba0908f3941356a62284e74727
Aref Miri Rekavandi <101369948+arekavandi@users.noreply.github.com>
2025-03-09 22:24:26 +0000

    Update utils_gradients.py

--- functions/utils_gradients.py
+++ functions/utils_gradients.py
@@ -586,7 +586,7 @@ def visualize_evaluate_embedding(embedded,Lowrank_DC,Dense_C_res,Dense_C,colorco
     print('MSE (grad_dense vs dense_res):',np.mean((Dense_C_res[np.triu_indices(Lowrank_DC.shape[0],k=1)] - temp1[np.triu_indices(Lowrank_DC.shape[0],k=1)]) ** 2))
     print('MSE (approx_dense vs Dense_C):',np.mean((Dense_C[np.triu_indices(Lowrank_DC.shape[0],k=1)] - approx_dense[np.triu_indices(Lowrank_DC.shape[0],k=1)]) ** 2))
 
-return temp1, approx_dense
+    return temp1, approx_dense
     
 '''def matrix_to_pmap_old(matrix, Nv1, Nv2, cycles):
     """
